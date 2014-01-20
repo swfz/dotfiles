22703e9664f167422b6dbd902a51ff143b19d092
swfz <sawafuji.09@gmail.com>
2014-01-20 23:44:43 +0900

    * tmux-powerline

--- setup.sh
+++ setup.sh
@@ -1,18 +1,53 @@
 #!/usr/bin/sh
 
-mkdir -p ~/.vim/bundle
-git clone git://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+DIRECTORY="~/.vim/bundle ~/bin ~/.tmux"
+
+for dir in $DIRECTORY
+do
+  if [ ! -e $dir ]; then
+    mkdir -p $dir
+  fi
+done
+
+if [ ! -e ~/.vim/bundle/neobundle.vim ]; then
+  git clone git://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+fi
 
 DOTFILES=".tmux.conf .vimrc"
 VIMDIRS="snippets dict"
 
 for file in $DOTFILES
 do
-  ln -s $HOME/dotfiles/$file $HOME/$file
+  if [ ! -L ~/$file ]; then
+    ln -s ~/dotfiles/$file ~/$file
+  fi
 done
 
-for file in $VIMDIRS
+for dir in $VIMDIRS
 do
-  ln -s $HOME/dotfiles/.vim/$file $HOME/.vim/$file
+  if [ ! -L ~/.vim/$dir ]; then
+    ln -s ~/dotfiles/.vim/$dir ~/.vim/$dir
+  fi
 done
+
+#tmux powerline
+
+if [ ! -e ~/.tmux/tmux-powerline ]; then
+  git clone https://github.com/erikw/tmux-powerline.git ~/.tmux/tmux-powerline
+  ln -s ~/dotfiles/tmux/lucius.sh ~/.tmux/tmux-powerline/themes/lucius.sh
+  ln -s ~/dotfiles/tmux/.tmux-powerlinerc ~/.tmux-powerlinerc
+
+cat << EOT >> ~/.bashrc
+PS1="\$PS1"'\$([ -n "\$TMUX" ] && tmux setenv TMUXPWD_\$(tmux display -p "#D" | tr -d %) "\$PWD")'
+export TERM=xterm-256color
+EOT
+
+fi
+
+if [ ! -e ~/bin/used-mem ]; then
+  git clone https://github.com/yonchu/used-mem.git ~/bin/used-mem
+  ln -s ~/bin/used-mem/used-mem ~/.tmux/tmux-powerline/segments/used-mem.sh
+  sed -i -e "s/.*main.*exit_usage.*//g" ~/bin/used-mem/used-mem
+  sed -i -e "s/main/run_segment/g" ~/bin/used-mem/used-mem
+fi
 
