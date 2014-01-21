5be931c474cf825352dbf7492eecfc7cfb6a6c63
swfz <sawafuji.yuya@localhost.localdomain>
2014-01-21 11:39:02 +0900

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/sh
 
-SHELLFILE="~/.bashrc"
-DIRECTORY="~/.vim/bundle ~/bin ~/.tmux"
+SHELLFILE=".bashrc"
+DIRECTORY=".vim/bundle bin .tmux"
 
 for dir in $DIRECTORY
 do
-  if [ ! -e $dir ]; then
-    mkdir -p $dir
+  if [ ! -e $HOME/$dir ]; then
+    mkdir -p $HOME/$dir
   fi
 done
 
-if [ ! -e ~/.vim/bundle/neobundle.vim ]; then
-  git clone git://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+if [ ! -e $HOME/.vim/bundle/neobundle.vim ]; then
+  git clone git://github.com/Shougo/neobundle.vim.git $HOME/vim/bundle/neobundle.vim
 fi
 
 DOTFILES=".tmux.conf .vimrc"
@@ -19,36 +19,35 @@ VIMDIRS="snippets dict"
 
 for file in $DOTFILES
 do
-  if [ ! -L ~/$file ]; then
-    ln -s ~/dotfiles/$file ~/$file
+  if [ ! -L $HOME/$file ]; then
+    ln -s $HOME/dotfiles/$file $HOME/$file
   fi
 done
 
 for dir in $VIMDIRS
 do
-  if [ ! -L ~/.vim/$dir ]; then
-    ln -s ~/dotfiles/.vim/$dir ~/.vim/$dir
+  if [ ! -L $HOME/.vim/$dir ]; then
+    ln -s $HOME/dotfiles/.vim/$dir $HOME/.vim/$dir
   fi
 done
 
 #tmux powerline
 
-if [ ! -e ~/.tmux/tmux-powerline ]; then
-  git clone https://github.com/erikw/tmux-powerline.git ~/.tmux/tmux-powerline
-  ln -s ~/dotfiles/tmux/lucius.sh ~/.tmux/tmux-powerline/themes/lucius.sh
-  ln -s ~/dotfiles/tmux/.tmux-powerlinerc ~/.tmux-powerlinerc
-
-cat << EOT >> $SHELLFILE
+if [ ! -e $HOME/.tmux/tmux-powerline ]; then
+  git clone https://github.com/erikw/tmux-powerline.git $HOME/.tmux/tmux-powerline
+  ln -s $HOME/dotfiles/tmux/lucius.sh $HOME/.tmux/tmux-powerline/themes/lucius.sh
+  ln -s $HOME/dotfiles/tmux/.tmux-powerlinerc $HOME/.tmux-powerlinerc
+cat << EOT >> $HOME/$SHELLFILE
 PS1="\$PS1"'\$([ -n "\$TMUX" ] && tmux setenv TMUXPWD_\$(tmux display -p "#D" | tr -d %) "\$PWD")'
 export TERM=xterm-256color
 EOT
-source $SHELLFILE
+source $HOME/$SHELLFILE
 fi
 
-if [ ! -e ~/bin/used-mem ]; then
-  git clone https://github.com/yonchu/used-mem.git ~/bin/used-mem
-  ln -s ~/bin/used-mem/used-mem ~/.tmux/tmux-powerline/segments/used-mem.sh
-  sed -i -e "s/.*main.*exit_usage.*//g" ~/bin/used-mem/used-mem
-  sed -i -e "s/main/run_segment/g" ~/bin/used-mem/used-mem
+if [ ! -e $HOME/bin/used-mem ]; then
+  git clone https://github.com/yonchu/used-mem.git $HOME/bin/used-mem
+  ln -s $HOME/bin/used-mem/used-mem $HOME/.tmux/tmux-powerline/segments/used-mem.sh
+  sed -i -e "s/.*main.*exit_usage.*//g" $HOME/bin/used-mem/used-mem
+  sed -i -e "s/main/run_segment/g" $HOME/bin/used-mem/used-mem
 fi
 
