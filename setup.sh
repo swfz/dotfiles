8d321b7febb18e05016cf164f79713aa3ce4708a
swfz <sawafuji.09@gmail.com>
2014-07-15 15:44:06 +0000

    * function

--- setup.sh
+++ setup.sh
@@ -28,89 +28,117 @@ function install_env() {
   fi
 }
 
+function set_env(){
+  exist_version=`$1 versions | grep $2`
+  if [ "$exist_version" = 0 ]; then
+    $1 install $2
+    $1 global $2
+  fi
+}
+
 #git config
-git config --global user.name swfz
-git config --global user.email sawafuji.09@gmail.com
-git config --global url."https://".insteadOf git://
-git config --global http.sslVerify false
-git config --global color.ui true
-git config --global core.editor vim
-
-install_anyenv
-install_env plenv
-install_env rbenv
-install_env ndenv
-install_env pyenv
-
-pyenv install 3.4.0
-pyenv global 3.4.0
-
-SHELLFILE=".zshrc"
-DIRECTORY=".vim/bundle bin .tmux"
-
-for dir in $DIRECTORY
-do
-  if [ ! -e $HOME/$dir ]; then
-    mkdir -p $HOME/$dir
+function git_config(){
+  exist_config=`git config  --get core.editor`
+  if [ "$exist_config" != "vim" ]; then
+    git config --global user.name swfz
+    git config --global user.email sawafuji.09@gmail.com
+    git config --global url."https://".insteadOf git://
+    git config --global http.sslVerify false
+    git config --global color.ui true
+    git config --global core.editor vim
   fi
-done
+}
 
-if [ ! -e $HOME/.vim/bundle/neobundle.vim ]; then
-  git clone git://github.com/Shougo/neobundle.vim.git $HOME/.vim/bundle/neobundle.vim
-fi
+function link_dotfiles(){
+  SHELLFILE=".zshrc"
+  DIRECTORY=".vim/bundle bin .tmux"
 
-DOTFILES=".tmux.conf .vimrc .zshrc"
-VIMDIRS="snippets dict"
+  for dir in $DIRECTORY
+  do
+    if [ ! -e $HOME/$dir ]; then
+      mkdir -p $HOME/$dir
+    fi
+  done
 
-for file in $DOTFILES
-do
-  if [ ! -L $HOME/$file ]; then
-    ln -s $HOME/dotfiles/$file $HOME/$file
+  if [ ! -e $HOME/.vim/bundle/neobundle.vim ]; then
+    git clone git://github.com/Shougo/neobundle.vim.git $HOME/.vim/bundle/neobundle.vim
   fi
-done
 
-for dir in $VIMDIRS
-do
-  if [ ! -L $HOME/.vim/$dir ]; then
-    ln -s $HOME/dotfiles/.vim/$dir $HOME/.vim/$dir
-  fi
-done
+  DOTFILES=".tmux.conf .vimrc .zshrc"
+  VIMDIRS="snippets dict"
+
+  for file in $DOTFILES
+  do
+    if [ ! -L $HOME/$file ]; then
+      ln -s $HOME/dotfiles/$file $HOME/$file
+    fi
+  done
+
+  for dir in $VIMDIRS
+  do
+    if [ ! -L $HOME/.vim/$dir ]; then
+      ln -s $HOME/dotfiles/.vim/$dir $HOME/.vim/$dir
+    fi
+  done
+}
 
 #tmux powerline
-if [ ! -e $HOME/.tmux/tmux-powerline ]; then
-  echo -e "\e[32m tmux powerline install..........\e[m"
-  git clone https://github.com/erikw/tmux-powerline.git $HOME/.tmux/tmux-powerline
-  ln -s $HOME/dotfiles/tmux/lucius.sh $HOME/.tmux/tmux-powerline/themes/lucius.sh
-  ln -s $HOME/dotfiles/tmux/blue.sh $HOME/.tmux/tmux-powerline/themes/blue.sh
-  ln -s $HOME/dotfiles/tmux/.tmux-powerlinerc $HOME/.tmux-powerlinerc
+install_tmux_powerline(){
+  if [ ! -e $HOME/.tmux/tmux-powerline ]; then
+    echo -e "\e[32m tmux powerline install..........\e[m"
+    git clone https://github.com/erikw/tmux-powerline.git $HOME/.tmux/tmux-powerline
+    ln -s $HOME/dotfiles/tmux/lucius.sh $HOME/.tmux/tmux-powerline/themes/lucius.sh
+    ln -s $HOME/dotfiles/tmux/blue.sh $HOME/.tmux/tmux-powerline/themes/blue.sh
+    ln -s $HOME/dotfiles/tmux/.tmux-powerlinerc $HOME/.tmux-powerlinerc
 cat << EOT >> $HOME/$PROFILE
 export TERM=xterm-256color
 EOT
 shell=`echo $SHELL`
 
-  case $shell in
-    *bash)
+    case $shell in
+      *bash)
 cat << EOT >> $HOME/$PROFILE
 PS1="\$PS1"'\$([ -n "\$TMUX" ] && tmux setenv TMUXPWD_\$(tmux display -p "#D" | tr -d %) "\$PWD")'
 EOT
-    ;;
-  esac
+      ;;
+    esac
 
-  source $HOME/$PROFILE
-fi
+    source $HOME/$PROFILE
+  fi
 
-if [ ! -e $HOME/bin/used-mem ]; then
-  git clone https://github.com/yonchu/used-mem.git $HOME/bin/used-mem
-  ln -s $HOME/bin/used-mem/used-mem $HOME/.tmux/tmux-powerline/segments/used-mem.sh
-  sed -i -e "s/.*main.*exit_usage.*//g" $HOME/bin/used-mem/used-mem
-  sed -i -e "s/main/run_segment/g" $HOME/bin/used-mem/used-mem
-fi
+  if [ ! -e $HOME/bin/used-mem ]; then
+    git clone https://github.com/yonchu/used-mem.git $HOME/bin/used-mem
+    ln -s $HOME/bin/used-mem/used-mem $HOME/.tmux/tmux-powerline/segments/used-mem.sh
+    sed -i -e "s/.*main.*exit_usage.*//g" $HOME/bin/used-mem/used-mem
+    sed -i -e "s/main/run_segment/g" $HOME/bin/used-mem/used-mem
+  fi
+}
 
-if [[ "$1" =~ "scheme" ]]; then
-  mv $HOME/.vim/bundle/powerline/powerline/config_files/colorschemes/shell/default.json $HOME/.vim/bundle/powerline/powerline/config_files/colorschemes/shell/default_back.json
-  ln -s $HOME/dotfiles/powerline_theme/shell_colorscheme_allblue.json $HOME/.vim/bundle/powerline/powerline/config_files/colorschemes/shell/default.json
-  mv $HOME/.vim/bundle/powerline/powerline/config_files/colorschemes/vim/default.json $HOME/.vim/bundle/powerline/powerline/config_files/colorschemes/vim/default_back.json
-  ln -s $HOME/dotfiles/powerline_theme/vim_colorscheme_allblue.json $HOME/.vim/bundle/powerline/powerline/config_files/colorschemes/vim/default.json
+git_config
+link_dotfiles
+
+install_anyenv
+envs="plenv rbenv ndenv pyenv"
+for env in $envs
+do
+  install_env $env
+done
+set_env pyenv 3.4.0
+
+install_tmux_powerline
+
+# set colorscheme
+if [[ "$#" -gt 1 ]]; then
+  powerline_config="$HOME/.vim/bundle/powerline/powerline/config_files/colorschemes"
+
+  if [ "$1" = "shell" -o "$1" = "vim" ]; then
+    if [ -L $powerline_config/$1/default.json ]; then
+      rm $powerline_config/$1/default.json
+    else
+      mv $powerline_config/$1/default.json $powerline_config/$1/default_back.json
+    fi
+    ln -s $HOME/dotfiles/powerline_theme/"$1"_colorscheme_"$2".json $powerline_config/$1/default.json
+  fi
 fi
 
 exec $SHELL -l
