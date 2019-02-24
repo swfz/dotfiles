ef3961c32c0f57a845d773ddfa2b75534ac5b830
swfz <sawafuji.09@gmail.com>
2019-02-24 17:28:05 +0900

    update command

--- batfiles/rubocop.bat
+++ batfiles/rubocop.bat
@@ -1,3 +1,3 @@
 @echo off
-bash -c "~/.anyenv/envs/rbenv/shims/rubocop %*"
+wsl ~/.anyenv/envs/rbenv/shims/rubocop $^(echo '%*' ^| sed -e 's^|\\^|/^|g' -e 's^|\^([A-Za-z]\^)\:/\^(.*\^)^|/mnt/\L\1\E/\2^|g'^)
 @echo on
