3bc1634dd48f5c60a4f1b5266355e43069c482ad
swfz <sawafuji.09@gmail.com>
2019-12-10 14:58:30 +0900

    fix: add install script

---
+++ ansible/roles/vim8/files/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+git clone https://github.com/vim/vim.git /tmp/vim
+
+cd /tmp/vim
+
+git checkout $1
+
+./configure \
+    --enable-fail-if-missing \
+    --with-features=huge \
+    --disable-selinux \
+    --enable-luainterp \
+    --enable-perlinterp \
+    --enable-cscope \
+    --enable-fontset \
+    --enable-multibyte
+
+make
+make install
+
+rm -rf /tmp/vim
