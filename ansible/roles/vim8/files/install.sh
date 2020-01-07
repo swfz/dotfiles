6464a38d7bb92f0555ff472315d7a63ff05035fa
swfz <sawafuji.09@gmail.com>
2020-01-07 12:30:26 +0900

    feat: add dependencies

--- ansible/roles/vim8/files/install.sh
+++ ansible/roles/vim8/files/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 git clone https://github.com/vim/vim.git /tmp/vim
 
@@ -7,15 +7,16 @@ cd /tmp/vim
 git checkout $1
 
 ./configure \
-    --enable-fail-if-missing \
-    --with-features=huge \
-    --disable-selinux \
-    --enable-luainterp \
-    --enable-perlinterp \
-    --enable-pythoninterp \
-    --enable-cscope \
-    --enable-fontset \
-    --enable-multibyte
+  --enable-fail-if-missing \
+  --with-features=huge \
+  --enable-gpm \
+  --disable-selinux \
+  --enable-luainterp \
+  --enable-perlinterp \
+  --enable-pythoninterp \
+  --enable-cscope \
+  --enable-fontset \
+  --enable-multibyte
 
 make
 make install
