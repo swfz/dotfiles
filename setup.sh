f0aea8d103b7f0dfeee13e89fb4ab9db6996f2ae
swfz <sawafuji.09@gmail.com>
2022-01-04 12:40:45 +0900

    feat: for codespace setup script

--- setup.sh
+++ setup.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
+# for codecpaces
 
-
-# ubuntu for codecpaces
-sudo apt update
-sudo apt install software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo python get-pip.py
-pip install pexpect --user
+sudo apt -y update
+sudo apt install -y python-pip python-setuptools
+sudo apt install -y ansible
+# TODO codespace用のインスタンスにansibleのロール(by_pip)を適用させたかったがcryptography内部で使っているようでrust compilierが必要らしい
+# そこまでしなくて良いという判断で、一旦cheatだけインストールする処理を入れる
+pytnon3 -m pip install cheat
 
 # codespace .zshrc is exist(oh-my-zsh).
 # I want to use my own file. saving existing .zshrc
 mv ~/.zshrc ~/.zshrc.bak
 
 cd ansible
-ansible-playbook -i localhost, -c local ubuntu.yml --extra-vars="user=$(whoami)"
+ansible-playbook -i localhost, -c local codespace.yml --extra-vars="user=$(whoami)"
